b297a06939749942f4283d291ef3d87e9c07d003
Yana Yakovleva <Yakovlevaya11.06@gmail.com>
2024-02-16 00:25:36 +0400

    tasks completed

--- lab_1/decryption_task.py
+++ lab_1/decryption_task.py
@@ -29,22 +29,28 @@ def replace_letters(text: str, freq_dict: dict, old_letter: str, new_letter: str
         logging.error(f"Error replace_letters {e}")
     return text
 
-
+def decryption(text_path: str, result_path: str, dict_path: str) -> None:
+    try:
+        text=read_file(text_path)
+        with open(dict_path, 'r', encoding='utf-8') as f:
+            dict=json.load(f)
+        for char, freq in dict.items():
+            text=text.replace(char,freq)
+        write_file(result_path, text)
+    except Exception as e:
+        logging.error(f"Error decryption {e}")
 
 if __name__ == "__main__":
     with open(os.path.join("lab_1", "settings.json"), 'r', encoding='utf-8') as json_f:
         settings = json.load(json_f)
-    
     text_path = os.path.join(settings["dir"], settings["folder"], settings["text"])
     freq_dict_path = os.path.join(settings["dir"], settings["folder"], settings["freq_dict"])
-    
-    with open(text_path, 'r', encoding='utf-8') as text_file:
+    """with open(text_path, 'r', encoding='utf-8') as text_file:
         text = text_file.read()
-    
     freq_dict = get_freq(text_path, freq_dict_path)
-    
     while True:
         print(text)
         old_letter = input("Enter the letter to replace\n")
         new_letter = input('Enter the new letter: ')
-        text = replace_letters(text, freq_dict, old_letter, new_letter, freq_dict_path)
+        text = replace_letters(text, freq_dict, old_letter, new_letter, freq_dict_path)"""
+    decryption(text_path, os.path.join(settings["dir"], settings["folder"], settings["result"]),freq_dict_path)
