c839afc80b7fff09b75958bb87ca57db96aa3a75
Yana Yakovleva <Yakovlevaya11.06@gmail.com>
2024-03-10 22:38:09 +0400

    I did test 3, but the sequence does not pass the test

--- lab_2/nist.py
+++ lab_2/nist.py
@@ -3,8 +3,11 @@ import logging
 import os
 import math
 
+from mpmath import gammainc
 
 logging.basicConfig(filename="lab_2/report.log", filemode="a", level=logging.INFO)
+
+PI={'v1':0.2148,'v2':0.3672,'v3':0.2305,'v4':0.1875}
 
 
 def freq_bit_test(sequence: str) -> float:
@@ -18,7 +21,55 @@ def freq_bit_test(sequence: str) -> float:
     return p_value
 
 
+def search_identical_bits(sequence: str) -> float:
+    try:
+        share=sum(int(bit) for bit in sequence)/len(sequence)
+        if abs(share-0.5)<(2/math.sqrt(len(sequence))):
+            v=0
+            for bit in range(len(sequence)-1):
+                if sequence[bit] != sequence[bit+1]:
+                    v+=1
+                p_value=math.erfc((abs(v-2*len(sequence)*share*(1-share)))/(2*math.sqrt(2*len(sequence))*(1-share)))
+        else: p_value=0
+    except Exception as e:
+        logging.error('Problem is in the test for identical bits')
+    return p_value
 
+
+def longest_sequence_test(sequence: str) -> float:
+    try:
+        blokcs=[sequence[i:i+8] for i in range(0,len(sequence), 8)]
+        statistic={'v1':0,'v2':0,'v3':0,'v4':0}
+        try:
+            for block in blokcs:
+                max_counter=0
+                counter=0
+                for bit in block:
+                    if bit=='1':
+                        counter+=1
+                    else:
+                        max_counter=max(max_counter,counter)
+                match max_counter:
+                    case 0,1:
+                        statistic['v1']+=1
+                    case 2:
+                        statistic['v2']+=1
+                    case 3:
+                        statistic['v3']+=1
+                    case _:
+                        statistic['v4']+=1
+            logging.info("Statistics collected")
+        except Exception as e:
+            logging.error("Statistics not collected")
+        chi_squared_distribution=0
+        for v in statistic:
+            chi_squared_distribution += (pow(statistic[v]-16*PI[v],2))/(16*PI[v])
+        print(chi_squared_distribution)
+        p_value=gammainc(3/2,pow(chi_squared_distribution,2)/2)
+    except Exception as e:
+        logging.error(f"Problem calculating chi-square and gamma function: {e}")
+    return p_value
+          
 
 def save_results(results, path: str) -> None:
     try:
@@ -34,4 +85,8 @@ if __name__ == "__main__":
         settings = json.load(json_f)
     save_results(freq_bit_test(settings['genc']),os.path.join(settings["folder"],settings["file"]))
     save_results(freq_bit_test(settings['genj']),os.path.join(settings["folder"],settings["file"]))
+    save_results(search_identical_bits(settings['genc']),os.path.join(settings["folder"],settings["file"]))
+    save_results(search_identical_bits(settings['genj']),os.path.join(settings["folder"],settings["file"]))
+    save_results(longest_sequence_test(settings['genc']),os.path.join(settings["folder"],settings["file"]))
+    save_results(longest_sequence_test(settings['genj']),os.path.join(settings["folder"],settings["file"]))
 
